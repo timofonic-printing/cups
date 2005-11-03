dbc4b522bfbc0e3130a953662811901a7df40cc2
kmuto <>
2005-11-03 12:52:22 +0000

    remove comments from init.d

--- debian/cupsys.init.d
+++ debian/cupsys.init.d
@@ -1,8 +1,5 @@
 #! /bin/sh
 #
-# cupsys	example file to build /etc/init.d/ scripts.
-#		This file should be used to construct scripts for /etc/init.d.
-#
 #		Written by Miquel van Smoorenburg <miquels@cistron.nl>.
 #		Modified for Debian GNU/Linux
 #		by Ian Murdock <imurdock@gnu.ai.mit.edu>.
