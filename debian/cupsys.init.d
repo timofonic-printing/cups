bd715009a9c2c9e8b746485c18645d46bc2ffb86
kmuto <>
2006-07-19 03:43:18 +0000

    remove unnecessary backslash

--- debian/cupsys.init.d
+++ debian/cupsys.init.d
@@ -30,7 +30,7 @@ case "$1" in
 	chown root:lpadmin /usr/share/cups/model 2>/dev/null || true
 	chmod 3775 /usr/share/cups/model 2>/dev/null || true
 	mkdir -p `dirname "$PIDFILE"`
-	if [ "$LOAD_LP_MODULE" = "yes" -a -f /usr/lib/cups/backend/parallel \\
+	if [ "$LOAD_LP_MODULE" = "yes" -a -f /usr/lib/cups/backend/parallel \
              -a -f /proc/devices -a -z "$(grep -e ' lp$' /proc/devices 2>/dev/null)" ]; then
 	    modprobe -q lp || true
           fi
