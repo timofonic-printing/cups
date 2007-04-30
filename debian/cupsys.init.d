e80958193927626dd5c37aa16e376c4024078f6b
mpitt <>
2007-04-30 13:52:05 +0000

    * Run the backend as non-root system user to confine the impact of security   vulnerabilities:   - Add debian/patches/10_external_pam_helper.dpatch:     + Add a helper program 'cups-check-pam-auth' which performs PAM       authentication and returns the status as exit code.     + scheduler/auth.c, cupsdAuthorize(): Attempt to use       cups-check-pam-auth before trying native PAM.     + With this we do not need to put the cupsys user into the shadow group.   - debian/cupsys.files: Install PAM helper.   - debian/cupsys.postinst: Set permissions of PAM helper to cupsys:shadow     2754, alter permissions of some configuration files and directories to     work with non-root cupsd.   - debian/cupsys.postinst: Fix /var/cache/cups/ permissions for upgrades     which might have written cache files as root.   - debian/cupsys.logrotate: Save log files as cupsys:lpadmin.   - debian/cupsys.init.d: Disable 'reload'; change force-reload to restart,     since cupsd cannot reload as non-root.   - debian/cupsys.init.d: Set up proper permissions of log files.   - debian/rules: Enable --enable-privilege-dropping configure option.   - debian/rules: Install lpd backend suid root, so that it can bind to     ports < 1024 (really silly, but required for RFC compliance).   - debian/cupsys-client.postinst, debian/cupsys.postinst: Set up cupsys     system user.

--- debian/cupsys.init.d
+++ debian/cupsys.init.d
@@ -37,7 +37,22 @@ case "$1" in
 	  modprobe -q lp || true
 	  modprobe -q ppdev || true
 	fi
+	chown cupsys:lp `dirname "$PIDFILE"`
+
+	# create the logs file since cupsd can't
+	for l in access_log page_log error_log; do
+	    [ -e /var/log/cups/$l ] || touch /var/log/cups/$l
+	    chmod 640 /var/log/cups/$l
+	    chown cupsys:lpadmin /var/log/cups/$l
+	done
+
 	start-stop-daemon --start --quiet --oknodo --pidfile "$PIDFILE" --exec $DAEMON
+
+	# Correct the permissions after starting the CUPS daemon
+	for l in access_log page_log error_log; do
+	    chmod 640 /var/log/cups/$l || true
+	    chown cupsys:lpadmin /var/log/cups/$l || true
+	done
 	log_end_msg $?
 	;;
   stop)
@@ -45,15 +60,15 @@ case "$1" in
 	start-stop-daemon --stop --quiet --retry 5 --oknodo --pidfile $PIDFILE --name $NAME
 	log_end_msg $?
 	;;
-  reload|force-reload)
-	log_begin_msg "Reloading $DESC: $NAME"
-	start-stop-daemon --stop --quiet --pidfile $PIDFILE --name $NAME --signal 1
-	log_end_msg $?
-	;;
-  restart)
+  restart|force-reload)
 	log_begin_msg "Restarting $DESC: $NAME"
 	if start-stop-daemon --stop --quiet --retry 5 --oknodo --pidfile $PIDFILE --name $NAME; then
 		start-stop-daemon --start --quiet --pidfile "$PIDFILE" --exec $DAEMON
+		# Correct the permissions after starting the CUPS daemon
+		for l in access_log page_log error_log; do
+		    chmod 640 /var/log/cups/$l || true
+		    chown cupsys:lpadmin /var/log/cups/$l || true
+		done
 	fi
 	log_end_msg $?
 	;;
