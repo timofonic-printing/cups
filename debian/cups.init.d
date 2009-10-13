972f3f2708f47d989c772685df02888d15c721c7
Till Kamppeter <till.kamppeter@gmail.com>
2009-10-13 22:54:25 +0200

    debian/cups.init.d: Make cold-plugging of USB printers also correctly work if the usblp kernel module is loaded.

--- debian/cups.init.d
+++ debian/cups.init.d
@@ -45,7 +45,9 @@ restart_xprint() {
 coldplug_usb_printers() {
     if type udevadm > /dev/null 2>&1 && [ -x /lib/udev/udev-configure-printer ]; then
 	for printer in `udevadm trigger --verbose --dry-run --subsystem-match=usb \
-		--attr-match=bInterfaceClass=07 --attr-match=bInterfaceSubClass=01`; do
+		--attr-match=bInterfaceClass=07 --attr-match=bInterfaceSubClass=01; \
+	                udevadm trigger --verbose --dry-run --subsystem-match=usb 
+		--sysname-match='lp[0-9]*'`; do
 	    /lib/udev/udev-configure-printer add "${printer#/sys}"
 	done
     fi
