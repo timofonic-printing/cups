4ac7b13f0fcd298ab227732651476453f42899ae
Martin Pitt <mpitt@debian.org>
2010-09-14 11:39:23 +0200

    debian/cups.init.d, debian/cups.postinst: Move custom PPD directory setup from init script into postinst. No need to do that on every boot.

--- debian/cups.init.d
+++ debian/cups.init.d
@@ -56,8 +56,6 @@ coldplug_usb_printers() {
 case "$1" in
   start)
 	log_begin_msg "Starting $DESC: $NAME"
-	chown root:lpadmin /usr/share/ppd/custom 2>/dev/null || true
-	chmod 3775 /usr/share/ppd/custom 2>/dev/null || true
 
 	mkdir -p `dirname "$PIDFILE"`
 	if [ "$LOAD_LP_MODULE" = "yes" -a -f /usr/lib/cups/backend/parallel \
