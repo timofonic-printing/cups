af26ad803e9a3ac97b3fc590bca344ca1a51f000
Till Kamppeter <till.kamppeter@gmail.com>
2011-02-08 11:00:44 +0100

    Corrected include in pdftoraster.cxx.

--- debian/local/filters/pdf-filters/filter/pdftoraster.cxx
+++ debian/local/filters/pdf-filters/filter/pdftoraster.cxx
@@ -43,7 +43,7 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include "Error.h"
 #include "GlobalParams.h"
 #include <cups/raster.h>
-#include <cups/image.h>
+#include <filter/image.h>
 #include <splash/SplashTypes.h>
 #include <splash/SplashBitmap.h>
 #include <strings.h>
