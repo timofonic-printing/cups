636736bf6b694603432da1f72f07754d29e1673d
Didier Raboud <odyx@debian.org>
2016-08-09 18:11:24 +0200

    Move cupsd.conf.default from SERVERROOT to DATADIR
    Origin: vendor
    Bug: https://cups.org/str.php?L4342
    Bug-Debian: http://bugs.debian.org/640124
    Last-Update: 2015-02-10
    
    Patch-Name: move-cupsd-conf-default-to-share.patch

--- cgi-bin/admin.c
+++ cgi-bin/admin.c
@@ -1922,6 +1922,7 @@ do_config_server(http_t *http)		/* I - HTTP connection */
     int		ch;			/* Character from file */
     char	filename[1024];		/* Filename */
     const char	*server_root;		/* Location of config files */
+    const char	*data_dir;		/* Location of data files */
 
 
    /*
@@ -2005,7 +2006,10 @@ do_config_server(http_t *http)		/* I - HTTP connection */
     * well...
     */
 
-    strlcat(filename, ".default", sizeof(filename));
+    if ((data_dir = getenv("CUPS_DATADIR")) == NULL)
+      data_dir = CUPS_DATADIR;
+
+    snprintf(filename, sizeof(filename), "%s/cupsd.conf.default",data_dir);
 
     if (!stat(filename, &info) && info.st_size < (1024 * 1024) &&
         (cupsd = cupsFileOpen(filename, "r")) != NULL)
