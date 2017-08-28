f45816e461fe3954b83eea1f72deb4809a333004
Didier Raboud <odyx@debian.org>
2017-08-28 09:25:00 +0200

    Let testlang use the source localization files, not the system ones

--- cups/testlang.c
+++ cups/testlang.c
@@ -19,6 +19,13 @@
 
 #include "cups-private.h"
 #include "ppd-private.h"
+#include <sys/stat.h>
+#ifdef WIN32
+#  include <io.h>
+#else
+#  include <unistd.h>
+#  include <fcntl.h>
+#endif /* WIN32 */
 #ifdef __APPLE__
 #  include <CoreFoundation/CoreFoundation.h>
 #endif /* __APPLE__ */
@@ -39,7 +46,10 @@ main(int  argc,				/* I - Number of command-line arguments */
   struct lconv		*loc;		/* Locale data */
   const char            *msgid,         /* String identifier */
                         *msgstr;        /* Localized string */
-  char			buffer[1024];	/* String buffer */
+  char			buffer[1024],	/* String buffers */
+                        localedir[1024],/* Directory for language locale file */
+                        realfilename[1024],/* Filename for language locale file */
+                        filepath[1024];	/* Filename for language locale file */
   double		number;		/* Number */
   static const char * const tests[] =	/* Test strings */
   {
@@ -63,6 +73,47 @@ main(int  argc,				/* I - Number of command-line arguments */
     setenv("LANG", argv[1], 1);
     setenv("SOFTWARE", "CUPS/" CUPS_SVERSION, 1);
   }
+
+  /*
+  * Setup directories for locale stuff...
+  */
+
+  if (access("locale", 0))
+  {
+    mkdir("locale", 0777);
+
+    snprintf(realfilename, sizeof(realfilename), "../locale/cups_%s.po", language->language);
+    snprintf(filepath, sizeof(filepath), "../../../locale/cups_%s.po", language->language);
+    snprintf(localedir, sizeof(localedir), "locale/%s", language->language);
+    snprintf(buffer, sizeof(buffer), "locale/%s/cups_%s.po", language->language, language->language);
+
+    if (strchr(language->language, '_') && access(realfilename, 0))
+    {
+      /*
+      * Country localization not available, look for generic localization...
+      */
+
+      snprintf(realfilename, sizeof(realfilename), "../locale/cups_%.2s.po", language->language);
+      snprintf(filepath, sizeof(filepath), "../../../locale/cups_%.2s.po", language->language);
+      snprintf(localedir, sizeof(localedir), "locale/%.2s", language->language);
+      snprintf(buffer, sizeof(buffer), "locale/%.2s/cups_%.2s.po", language->language, language->language);
+
+      if (access(realfilename, 0))
+      {
+          /*
+          * No generic localization, so use POSIX...
+          */
+          snprintf(filepath, sizeof(filepath), "../../../locale/cups_C.po");
+          snprintf(localedir, sizeof(localedir), "locale/C");
+          snprintf(buffer, sizeof(buffer), "locale/C/cups_C.po");
+        }
+    }
+
+    mkdir(localedir, 0777);
+    symlink(filepath, buffer);
+  }
+
+  putenv("LOCALEDIR=locale");
 
   _cupsSetLocale(argv);
 
